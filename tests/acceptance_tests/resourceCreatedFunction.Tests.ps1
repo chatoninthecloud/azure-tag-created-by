5b6e1a194ad2d19cf7f374adc3fd29a95ce11cbc
chaton <sylvain.martinez@ymail.com>
2019-08-16 16:26:35 +0200

    refacto

--- tests/acceptance_tests/resourceCreatedFunction.Tests.ps1
+++ tests/acceptance_tests/resourceCreatedFunction.Tests.ps1
@@ -1,12 +1,13 @@
 Describe "Created resources supporting Tag are tagged with their creator" -Tag @("acceptance_test") {
 
     BeforeAll { 
-        $resourceGroupName = "tag-test-rg"
-        $location = "NorthEurope"
-        $publicIPName = "testpublicip01"
-        $servicePrincipalId = "ee6e2b3b-3af5-4c2d-a0db-84f1b688c552"
-        $maxRetry = 20
-        $delay = 10
+        $configuration = Get-Content -Raw ".\tests\acceptance_tests\configuration.json" | ConvertFrom-Json        
+        $resourceGroupName = $configuration.resourceGroupName
+        $location = $configuration.location
+        $publicIPName = $configuration.publicIPName
+        $servicePrincipalId = $configuration.servicePrincipalId
+        $maxRetry = $configuration.maxRetry
+        $delay = $configuration.delay
         Write-Host "Creating test Resource Group $resourceGroupName"
         New-AzResourceGroup -Name $resourceGroupName -Location $location | Out-Null        
     }
@@ -59,7 +60,7 @@ Describe "Created resources supporting Tag are tagged with their creator" -Tag @
 
         It "The tag is added if it is removed" {                                  
             $publicIp = Get-AzResource -Name $publicIpName -ResourceGroupName $resourceGroupName
-            Set-AzResource -ResourceId $publicIp.ResourceId -Tag @{}
+            Set-AzResource -ResourceId $publicIp.ResourceId -Tag @{} -Force | Out-Null
             $tagCheck = Get-ResourceCreatedByTag -maxRetry $maxRetry -delay $delay -resourceName $publicIpName -resourceGroupName $resourceGroupName
             $tagCheck[0] | Should -Be $true
             $tagCheck[1] | Should -Be "Service Principal $servicePrincipalId"   
