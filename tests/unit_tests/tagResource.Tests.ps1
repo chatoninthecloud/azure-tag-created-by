ecd9878cd8bb302e02cfdddad3d6b38f091a7738
chaton <sylvain.martinez@ymail.com>
2019-08-20 01:43:15 +0200

    add InModuleScope

--- tests/unit_tests/tagResource.Tests.ps1
+++ tests/unit_tests/tagResource.Tests.ps1
@@ -1,4 +1,4 @@
-Describe "Tag resource module tests" -Tag @("tagResourceModule","unit_test") {
+Describe "Tag resource module tests" -Tag @("tagResourceModule", "unit_test") {
 
     BeforeAll {
         Import-Module .\function\Modules\tagResource -Force        
@@ -40,139 +40,160 @@ Describe "Tag resource module tests" -Tag @("tagResourceModule","unit_test") {
     Context "Get the event initiator display name" {
         it "Should retrieve the creator display name if created by a service principal" {
             $testQueueItem = @{data = @{
-                authorization = @{evidence= @{principalType="ServicePrincipal"}};
-                claims = @{appId="c'est moi lol"}}
+                    authorization = @{evidence = @{principalType = "ServicePrincipal" } };
+                    claims        = @{appId = "c'est moi lol" }
+                }
             }
             $displayName = Get-EventInitiatorDisplayName -queueItem $testQueueItem
             $displayName | Should -Be "Service Principal c'est moi lol"
         }
 
         it "Should retrieve the creator display name if created by a user" {
-            $testQueueItem = @{data = @{claims = @{name="c'est lui"}}
+            $testQueueItem = @{data = @{claims = @{name = "c'est lui" } }
             }
             $displayName = Get-EventInitiatorDisplayName -queueItem $testQueueItem
             $displayName | Should -Be "c'est lui"
         }
     }
 
-    Context "Apply the created tag to the resource" {
-
-        it "Should add the createdBy tag if resource has any tag" {
-            $testResourceTags = @{}
-            $resourceUri = "/subscription/bim"    
-            $creatorDisplayName = "Ambroise"                                
-            Mock Set-AzResource {return $null} -Verifiable -ParameterFilter {
-                $ResourceId -eq $resourceUri `
-                -and $Tag["createdBy"] -eq $creatorDisplayName
-            }         
-            Set-ResourceCreatedByTag -resourceTags $testResourceTags -creatorDisplayName $creatorDisplayName -resourceUri $resourceUri
-            Assert-VerifiableMock
+    Context "Add createdBy tag" {
+        InModuleScope "tagResource" {
+            it "Should add the createdBy tag if resource has any tag" {
+                $testResourceTags = @{ }
+                $resourceUri = "/subscription/bim"    
+                $creatorDisplayName = "Ambroise"                                
+                Mock Set-AzResource { return $null } -Verifiable -ParameterFilter {
+                    $ResourceId -eq $resourceUri `
+                        -and $Tag["createdBy"] -eq $creatorDisplayName
+                }         
+                Set-ResourceCreatedByTag -resourceTags $testResourceTags -creatorDisplayName $creatorDisplayName -resourceUri $resourceUri
+                Assert-VerifiableMock
+            }
         }
+    }
 
-        it "Should append the createdBy tag if resource already has tags" {
-            $testResourceTags = @{kikoo="bim"}
-            $resourceUri = "/subscription/bim"    
-            $creatorDisplayName = "Ambroise"                                
-            Mock Set-AzResource {return $null} -Verifiable -ParameterFilter {
-                $ResourceId -eq $resourceUri `
-                -and $Tag["createdBy"] -eq $creatorDisplayName `
-                -and $Tag["kikoo"] -eq "bim"
-            }         
-            Set-ResourceCreatedByTag -resourceTags $testResourceTags -creatorDisplayName $creatorDisplayName -resourceUri $resourceUri
-            Assert-VerifiableMock
+    Context "Append createdByTag" {
+        InModuleScope "tagResource" {
+
+            it "Should append the createdBy tag if resource already has tags" {
+                $testResourceTags = @{kikoo = "bim" }
+                $resourceUri = "/subscription/bim"    
+                $creatorDisplayName = "Ambroise"                                
+                Mock Set-AzResource { return $null } -Verifiable -ParameterFilter {
+                    $ResourceId -eq $resourceUri `
+                        -and $Tag["createdBy"] -eq $creatorDisplayName `
+                        -and $Tag["kikoo"] -eq "bim"
+                }         
+                Set-ResourceCreatedByTag -resourceTags $testResourceTags -creatorDisplayName $creatorDisplayName -resourceUri $resourceUri
+                Assert-VerifiableMock
+            }
+        } }
+
+    Context "Update createdBy Tag" {
+        InModuleScope "tagResource" {
+
+            it "Should update the createdBy tag if it has a wrong value" {
+                $testResourceTags = @{createdBy = "bim" }
+                $resourceUri = "/subscription/bim"    
+                $creatorDisplayName = "Ambroise"                                
+                Mock Set-AzResource { return $null } -Verifiable -ParameterFilter {
+                    $ResourceId -eq $resourceUri `
+                        -and $Tag["createdBy"] -eq $creatorDisplayName
+                }         
+                Set-ResourceCreatedByTag -resourceTags $testResourceTags -creatorDisplayName $creatorDisplayName -resourceUri $resourceUri
+                Assert-VerifiableMock
+            }
         }
+    }
 
-        it "Should update the createdBy tag if it has a wrong value" {
-            $testResourceTags = @{createdBy="bim"}
-            $resourceUri = "/subscription/bim"    
-            $creatorDisplayName = "Ambroise"                                
-            Mock Set-AzResource {return $null} -Verifiable -ParameterFilter {
-                $ResourceId -eq $resourceUri `
-                -and $Tag["createdBy"] -eq $creatorDisplayName
-            }         
-            Set-ResourceCreatedByTag -resourceTags $testResourceTags -creatorDisplayName $creatorDisplayName -resourceUri $resourceUri
-            Assert-VerifiableMock
-        }
+    Context "Update createdBy Tag" {
+        InModuleScope "tagResource" {
 
-        it "Should not update the createdBy tag if it already has the good value" {
-            $creatorDisplayName = "Ambroise" 
-            $testResourceTags = @{createdBy=$creatorDisplayName}
-            $resourceUri = "/subscription/bim"                         
-            Mock Set-AzResource -Verifiable  
-            Set-ResourceCreatedByTag -resourceTags $testResourceTags -creatorDisplayName $creatorDisplayName -resourceUri $resourceUri
-            Assert-MockCalled Set-AzResource -Times 0
+            it "Should not update the createdBy tag if it already has the good value" {
+                $creatorDisplayName = "Ambroise" 
+                $testResourceTags = @{createdBy = $creatorDisplayName }
+                $resourceUri = "/subscription/bim"                         
+                Mock Set-AzResource -Verifiable  
+                Set-ResourceCreatedByTag -resourceTags $testResourceTags -creatorDisplayName $creatorDisplayName -resourceUri $resourceUri
+                Assert-MockCalled Set-AzResource -Times 0
+            }
         }
     }
 
     Context "Update the resources creator referential" {
+        InModuleScope "tagResource" {
+            it "Should add the creator in the referential if it does not exist" {   
+                $resourceUri = "kiKKo/IamAResourCE/Nice" 
+                $formatedResourceUri = "kikko-iamaresource-nice" 
+                $subscriptionId = "46545645454"
+                $creatorDisplayName = "Gabriel"
+                $eventTime = "now"
+                $cloudTable = @{hello = "iamacloudtable" }                   
 
-        it "Should add the creator in the referential if it does not exist" {   
-            $resourceUri = "kiKKo/IamAResourCE/Nice" 
-            $formatedResourceUri = "kikko-iamaresource-nice" 
-            $subscriptionId = "46545645454"
-            $creatorDisplayName = "Gabriel"
-            $eventTime = "now"
-            $cloudTable = @{hello="iamacloudtable"}                   
-
-            $env:storageName = "theStorage"
-            $env:resourceGroupName = "theResourceGroup" 
-            $env:tableName = "theTableName"       
+                $env:storageName = "theStorage"
+                $env:resourceGroupName = "theResourceGroup" 
+                $env:tableName = "theTableName"       
         
-            Mock Get-AzTableTable -Verifiable {return $cloudTable} -ParameterFilter {
-                $storageAccountName -eq $env:storageName `
-                -and $resourceGroup -eq $env:resourceGroupName `
-                -and $TableName -eq $env:tableName 
-            }       
-            Mock Get-AzTableRow -Verifiable {return $null} -ParameterFilter {
-                $Table -eq $cloudTable `
-                -and $PartitionKey -eq  $formatedResourceUri `
-                -and $RowKey -eq $subscriptionId
+                Mock Get-AzTableTable -Verifiable { return $cloudTable } -ParameterFilter {
+                    $storageAccountName -eq $env:storageName `
+                        -and $resourceGroup -eq $env:resourceGroupName `
+                        -and $TableName -eq $env:tableName 
+                }       
+                Mock Get-AzTableRow -Verifiable { return $null } -ParameterFilter {
+                    $Table -eq $cloudTable `
+                        -and $PartitionKey -eq $formatedResourceUri `
+                        -and $RowKey -eq $subscriptionId
+                }
+
+                Mock Add-AzTableRow -Verifiable { return $null } -ParameterFilter {
+                    $Table -eq $cloudTable `
+                        -and $PartitionKey -eq $formatedResourceUri `
+                        -and $RowKey -eq $subscriptionId `
+                        -and $property["createdBy"] -eq $creatorDisplayName `
+                        -and $property["createdAt"] -eq $eventTime
+                } 
+
+                $displayName = Update-referential -resourceUri $resourceUri -subscriptionId $subscriptionId -creatorDisplayName $creatorDisplayName -eventTime $eventTime
+                $displayName | Should -Be $creatorDisplayName
+                Assert-VerifiableMock 
             }
-
-            Mock Add-AzTableRow -Verifiable {return $null} -ParameterFilter {
-                $Table -eq $cloudTable `
-                -and $PartitionKey -eq $formatedResourceUri `
-                -and $RowKey -eq $subscriptionId `
-                -and $property["createdBy"] -eq $creatorDisplayName `
-                -and $property["createdAt"] -eq $eventTime
-            } 
-
-            $displayName = Update-referential -resourceUri $resourceUri -subscriptionId $subscriptionId -creatorDisplayName $creatorDisplayName -eventTime $eventTime
-            $displayName | Should -Be $creatorDisplayName
-            Assert-VerifiableMock 
         }
+    }
 
-        it "Should get the creator from the referential if it exists" {   
-            $resourceUri = "kiKKo/IamAResourCE/Nice" 
-            $formatedResourceUri = "kikko-iamaresource-nice" 
-            $subscriptionId = "46545645454"
-            $creatorDisplayName = "Gabriel"
-            $eventTime = "now"
-            $cloudTable = @{hello="iamacloudtable"}                   
+    Context "Get resource creator from referential" {
+        InModuleScope "tagResource" {
 
-            $env:storageName = "theStorage"
-            $env:resourceGroupName = "theResourceGroup" 
-            $env:tableName = "theTableName"       
+            it "Should get the creator from the referential if it exists" {   
+                $resourceUri = "kiKKo/IamAResourCE/Nice" 
+                $formatedResourceUri = "kikko-iamaresource-nice" 
+                $subscriptionId = "46545645454"                
+                $eventTime = "now"
+                $cloudTable = @{hello = "iamacloudtable" }                   
+
+                $env:storageName = "theStorage"
+                $env:resourceGroupName = "theResourceGroup" 
+                $env:tableName = "theTableName"       
         
-            Mock Get-AzTableTable -Verifiable {return $cloudTable} -ParameterFilter {
-                $storageAccountName -eq $env:storageName `
-                -and $resourceGroup -eq $env:resourceGroupName `
-                -and $TableName -eq $env:tableName 
-            }       
-            Mock Get-AzTableRow -Verifiable {return @{createdBy=$creatorDisplayName}} -ParameterFilter {
-                $Table -eq $cloudTable `
-                -and $PartitionKey -eq  $formatedResourceUri `
-                -and $RowKey -eq $subscriptionId
-            }
+                Mock Get-AzTableTable -Verifiable { return $cloudTable } -ParameterFilter {
+                    $storageAccountName -eq $env:storageName `
+                        -and $resourceGroup -eq $env:resourceGroupName `
+                        -and $TableName -eq $env:tableName 
+                }       
+                Mock Get-AzTableRow -Verifiable { return @{createdBy = "Gabriel" } } -ParameterFilter {
+                    $Table -eq $cloudTable `
+                        -and $PartitionKey -eq $formatedResourceUri `
+                        -and $RowKey -eq $subscriptionId
+                }
 
-            Mock Add-AzTableRow -Verifiable
+                Mock Add-AzTableRow -Verifiable
 
-            $displayName = Update-referential -resourceUri $resourceUri -subscriptionId $subscriptionId -creatorDisplayName "Francis" -eventTime $eventTime
-            $displayName | Should -Be $creatorDisplayName
+                $displayName = Update-referential -resourceUri $resourceUri -subscriptionId $subscriptionId -creatorDisplayName "Francis" -eventTime $eventTime
+                $displayName | Should -Be "Gabriel"
             
-            Assert-MockCalled Add-AzTableRow -Times 0 
-            Assert-MockCalled Get-AzTableTable -Times 1
-            Assert-MockCalled Get-AzTableRow -Times 1            
+                Assert-MockCalled Add-AzTableRow -Times 0 
+                Assert-MockCalled Get-AzTableTable -Times 1
+                Assert-MockCalled Get-AzTableRow -Times 1            
+            }
         }
     }
 }
