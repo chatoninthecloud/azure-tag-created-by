ee52f339b77ebd6113cfed18fb02936c1501334d
chaton <sylvain.martinez@ymail.com>
2019-08-21 01:48:12 +0200

    add deployment files

---
+++ deployment/deploy_functions.ps1
@@ -0,0 +1,5 @@
+$password = $env:MAPPED_TERRAFORM_SP_SECRET | ConvertTo-SecureString -asPlainText -Force
+$credential = New-Object System.Management.Automation.PSCredential ($env:TERRAFORM_SP_APPLICATIONID, $password)
+Connect-AzAccount -ServicePrincipal -Tenant $env:TERRAFORM_SP_TENANTID -Credential $credential
+Get-AzSubscription -SubscriptionId $env:TERRAFORM_SP_SUBSCRIPTIONID | Select-AzSubscription
+func azure functionapp publish tagfunctionapp
---
+++ deployment/destroy_infra.sh
@@ -0,0 +1,8 @@
+export ARM_SUBSCRIPTION_ID=$TERRAFORM_SP_SUBSCRIPTIONID
+export ARM_TENANT_ID=$TERRAFORM_SP_TENANTID
+export ARM_CLIENT_ID=$TERRAFORM_SP_APPLICATIONID
+export ARM_CLIENT_SECRET=$MAPPED_TERRAFORM_SP_SECRET
+export ARM_ACCESS_KEY=$MAPPED_TERRAFORM_ACCESS_KEY
+cd ./terraform
+terraform init
+terraform destroy -var-file=./environment/dev.tfvars.json -auto-approve
