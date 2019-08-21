170b89087803c0eefa98c69123442d7f352c506e
Sylvain Martinez <42356970+chatoninthecloud@users.noreply.github.com>
2019-08-21 02:10:21 +0200

    Update deploy_functions.ps1

--- deployment/deploy_functions.ps1
+++ deployment/deploy_functions.ps1
@@ -1,6 +1,6 @@
 $password = $env:MAPPED_TERRAFORM_SP_SECRET | ConvertTo-SecureString -asPlainText -Force
 $credential = New-Object System.Management.Automation.PSCredential ($env:TERRAFORM_SP_APPLICATIONID, $password)
 Connect-AzAccount -ServicePrincipal -Tenant $env:TERRAFORM_SP_TENANTID -Credential $credential
-Get-AzSubscription -SubscriptionId $env:TERRAFORM_SP_SUBSCRIPTIONID | Select-AzSubscription
+Get-AzSubscription -SubscriptionId $env:TERRAFORM_SP_SUBSCRIPTIONID | Select-AzSubscription | Out-Null
 Set-Location ./function 
-func azure functionapp publish tagfunctionapp
+func azure functionapp publish tagfunctionapp --powershell
